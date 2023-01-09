ca2e1b272a336b2ed69bae98dc618d81a0c693a8
Tom Radford <tom@theradford.com>
2023-01-09 16:43:48 +0200

    astro setup and animations init

--- script.js
+++
@@ -1,9 +0,0 @@
-const trigger = document.querySelector("#trigger");
-const wee = document.querySelector(".flyer");
-trigger.addEventListener("click", (e) => {
-  if (wee.className.includes("flyer-move")) {
-    wee.className = `flyer`;
-  } else {
-    wee.className = `${wee.className} flyer-move`;
-  }
-});
